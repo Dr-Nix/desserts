5946b71308353476a855f1a39dadece9b9afda53
Boris Hupkens van der Elst <b.hupkens@gmail.com>
2018-12-30 11:36:08 +0100

    check_laatste

--- python/speelveld.py
+++ python/speelveld.py
@@ -26,7 +26,22 @@ class Speelveld:
         pass
 
     def check_laatste(self):
-        pass
+        curtegel = len(self.tegels) - 1
+        row = curtegel // self.cols
+        col = curtegel % self.cols
+        valid = True
+        if row > 0:
+            valid = self.tegels[curtegel].opdruk_int[0] == self.tegels[curtegel - self.cols].opdruk_int[2]
+            # print('checking tegel {}, rows: {}=={}'.format(curtegel,
+            #                                               self.tegels[curtegel].opdruk_int[0],
+            #                                               self.tegels[curtegel - self.cols].opdruk_int[2]))
+        if col > 0:
+            valid &= self.tegels[curtegel].opdruk_int[3] == self.tegels[curtegel - 1].opdruk_int[1]
+            # print('checking tegel {}, cols: {}=={}'.format(curtegel,
+            #                                               self.tegels[curtegel].opdruk_int[3],
+            #                                               self.tegels[curtegel - 1].opdruk_int[1]))
+        # print('check valid = ' + str(valid))
+        return valid
 
     def __str__(self):
         s = []
@@ -41,4 +56,6 @@ if __name__ == '__main__':
     veld = Speelveld(2, 2)
     for T in tegels.set4(shuffle=True, seed=1):
         veld.add_tegel(T)
+        # print(veld)
+        veld.check_laatste()
     print(veld)
