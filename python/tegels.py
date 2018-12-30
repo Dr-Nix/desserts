b0bff837f43726bdf43ed8870b85099cfc37302f
Boris Hupkens van der Elst <b.hupkens@gmail.com>
2018-12-30 01:26:31 +0100

    rotatie, standaardset

--- python/tegels.py
+++ python/tegels.py
@@ -22,13 +22,43 @@ class Tegel:
     def __str__(self):
         return 'Tegel' + str(self.opdruk) + ' ' + str(self.opdruk_int)
 
+    def __repr__(self):
+        return 'Tegel' + str(self.opdruk)
+
     def to_int(self):
         try:
             self.opdruk_int = tuple(getattr(self, soort) for soort in self.opdruk)
         except AttributeError as ae:
             raise NameError('Invalid dessert specified: ' + str(ae))
 
+    def rotate(self, turns):
+        turns = turns % 4
+        new_opdruk = self.opdruk[turns:] + self.opdruk[:turns]
+        return Tegel(new_opdruk)
+
+
+def standaardSet():
+    lijst = ('muffin soesje softijs ijsschaal',
+             'muffin taart softijs ijsschaal',
+             'softijs ijsschaal muffin taart')
+    return [Tegel(t.split()) for t in lijst]
+
+
+def set4():
+    lijst = ('muffin taart softijs ijsschaal',
+             'softijs ijsschaal muffin taart',
+             'softijs ijsschaal muffin soesje',
+             'muffin softijs rode_coupe ijsschaal')
+    return [Tegel(t.split()) for t in lijst]
+
 
 if __name__ == '__main__':
-    tegel = Tegel(('muffine', 'soesje', 'softijs', 'ijsschaal'))
-    print(tegel)
+    tegel = Tegel(('muffin', 'soesje', 'softijs', 'ijsschaal'))
+    print(tegel.rotate(-1))
+    print(tegel.rotate(0))
+    print(tegel.rotate(1))
+    print(tegel.rotate(2))
+    print(tegel.rotate(3))
+    print(tegel.rotate(4))
+    a = set4()
+    print(a)
