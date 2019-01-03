840334a3ca8fc0e30fbe4a87826c4e61a038595a
Boris Hupkens van der Elst <b.hupkens@gmail.com>
2019-01-03 17:05:45 +0100

    basic c++ project

---
+++ cpp/src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : desserts-cpp.cpp
+// Author      : Boris
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//#include <iostream>
+//#include <string>
+
+int main() {
+    int s = 13;
+	return 0;
+}
